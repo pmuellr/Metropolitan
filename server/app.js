bc49be028b5113581bdbe6c02da0a914684cd2a4
Patrick Mueller <pmuellr@apache.org>
2014-04-28 16:29:32 -0400

    replace phone nos with templates

--- server/app.js
+++ server/app.js
@@ -1,14 +1,14 @@
 var app = require('gopher'),
     twilio = require('twilio');
 
-var twilioNumber = '(919) 926-0835';
+var twilioNumber = '(ABC) DEF-GHIJ';
 
 // Jeff quickie demo
 app.get('/', function(request, response) {
     var client = twilio();
 
     client.sendMessage({
-        to:'+19192448670', //this should be your own phone
+        to:'+1JIHGFEDCBA', //this should be your own phone
         from:twilioNumber,
         body:'Yo Impact! What up homies!'
     }, function(err, message) {
