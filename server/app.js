018d0078998a8272aeb69a321072d870429769f5
Patrick Mueller <pmuellr@apache.org>
2014-04-28 16:20:43 -0400

    add an error check

--- server/app.js
+++ server/app.js
@@ -1,17 +1,21 @@
 var app = require('gopher'),
     twilio = require('twilio');
 
-var twilioNumber = '(612) 260-5740';
+var twilioNumber = '(919) 926-0835';
 
 // Jeff quickie demo
 app.get('/', function(request, response) {
     var client = twilio();
 
     client.sendMessage({
-        to:'+16515556666', //this should be your own phone
+        to:'+19192448670', //this should be your own phone
         from:twilioNumber,
-        body:'Yo Pulse! What up homies!'
+        body:'Yo Impact! What up homies!'
     }, function(err, message) {
+    		if (err) {
+    			console.log("error sending message: ", err)
+    			return;
+    		}
         response.send('Message sent: '+message.sid);
     });
 
